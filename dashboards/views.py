e217a5707a42d5a82ed3750ca8c17b4249f756ed
jainil <jainiltrivedi54@gmail.com>
2026-02-26 12:43:17 +0530

    Add role_required decorator and unauthorized access page

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .decorators import role_required
 
 # Create your views here.
-@login_required(login_url="login")
+#@login_required(login_url="login")
+@role_required(allowed_roles=["admin"])
 def adminDashboardView(request):
     return render(request,"dashboards/admin_dashboard.html")
 
-@login_required(login_url="login")
+#@login_required(login_url="login")
+@role_required(allowed_roles=["manager"])
 def managerDashboardView(request):
     return render(request,"dashboards/manager_dashboard.html")
 
-@login_required(login_url="login")
+#@login_required(login_url="login")
+@role_required(allowed_roles=["developer"])
 def developerDashboardView(request):
     return render(request,"dashboards/developer_dashboard.html")
 
-@login_required(login_url="login")
+#@login_required(login_url="login")
+@role_required(allowed_roles=["tester"])
 def testerDashboardView(request):
     return render(request,"dashboards/tester_dashboard.html")
